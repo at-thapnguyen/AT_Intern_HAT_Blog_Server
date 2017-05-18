4e1f0efb3135c9d4761f68eb84945a94c3f665aa
at-thapnguyen <thapnguyen@asiantech.vn>
2017-05-18 16:20:53 +0700

    CRU ARTICLES

--- blog-app/app/controllers/api/v1/articles_controller.rb
+++ blog-app/app/controllers/api/v1/articles_controller.rb
@@ -1,13 +1,59 @@
-class Api::V1::ArticlesController < ApplicationController
-	def index
-		# article = Article.page(params[:page] ? params[:page][:number] : 1)
-		render json: Article.all,meta: {status:400}
+class Api::V1::ArticlesController < BaseController 
+  
+  def index
+    # binding.pry
+    articles = Article.all.includes(:comments, :attentions, :user)
+    render json: articles, each_serializer: Article::ArticleSerializer, meta: { total: articles.count, limit: 10 }
+  end
 
-	end
+  def show
+    render json: Article.find(params[:id]), serializer: ShowArticleSerializer
+  end
 
-	def show
-		render json: Article.find(params[:id])
-	end
+  def create  
+    user = check_login response.request.env["HTTP_ACCESS_TOKEN"]
+    if !user.blank?
+      article = Article.new(article_params)
+      # params[:article][:title_image].original_filename = rename_file params[:article][:title_image].original_filename
+      article.title = params[:article][:title]
+      article.content = params[:article][:content]
+      article.title_image = params[:article][:title_image]
+      article.category_id = params[:article][:category_id]
+      
+      article.user_id = user.id
+       article.save 
+        render json: {status: 200}
+        else
+        render json: { status: "unsuccess",message:"you must confirm email",code: 406 }
+        end
+    end
 
-	end
+    def update
+      user = check_login response.request.env["HTTP_ACCESS_TOKEN"]
+      if !user.blank?
+        article = Article.find(params[:id])
+        article.update(article_params)
+        article.title = params[:article][:title]
+        article.content = params[:article][:content]
+        article.title_image = params[:article][:title_image]
+        article.category_id = params[:article][:category_id]
+        article.user_id = user.id
+          render json: {status: 200}
+      else
+          render json: { status: "unsuccess",message:"you must confirm email",code: 406 }
+      end  
+    end
+
+  private
+
+  def article_params
+    params.require(:article).permit :title,:content,:title_image,
+    :category_id,:createDay,:user_id,:deleted
+  end
+
+  def rename_file filename, change_name
+    change_name.to_s + filename[filename.rindex(/\./)..filename.size].downcase
+  end
+end
+
 
