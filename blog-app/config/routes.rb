5da0ce91dfd86ae985c89b498c0807d2c648be7c
at-huynguyen2 <huy.nguyen2@asiantech.vn>
2017-06-02 10:31:30 +0700

    fix follow user controller

--- blog-app/config/routes.rb
+++ blog-app/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       #Huy developer
       resources :authorizations, except: [:index, :new, :edit]
@@ -8,16 +8,17 @@ Rails.application.routes.draw do
         resources :follow_users, only: [:index, :show]
       end
       resources :tags, except: [:new, :edit]
-
+      resources :categories, except: [:new, :edit]
       resources :notifications, only: [:show]
-      #Thap developer
-      resources :categories, except: [:new, :edit] do
-        resources :articles, except: [:new, :edit] do
-          resources :comments,only: [:index,:create,:show,:update,:destroy]
-          resources :likes, only: [:create]
-          resources :follows, only: [:create]
-        end
+      namespace :articles do
         resources :hot_articles, only: [:index]
+      end
+
+      #Thap developer
+      resources :articles,only: [:index, :create, :update, :show, :destroy] do
+        resources :comments,only: [:index,:create,:show,:update,:destroy]
+        resources :likes, only: [:create]
+        resources :follows, only: [:create]
       end
       resources :tags
     end
